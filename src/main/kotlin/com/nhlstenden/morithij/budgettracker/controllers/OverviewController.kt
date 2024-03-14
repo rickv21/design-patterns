d794f35b71172add92f7ca1709683379188e43d8
Driftkikker32 <487851@student.saxion.nl>
2024-03-14 16:33:55 +0100

    added threads to get DAO of the main thread

--- src/main/kotlin/com/nhlstenden/morithij/budgettracker/controllers/OverviewController.kt
+++ src/main/kotlin/com/nhlstenden/morithij/budgettracker/controllers/OverviewController.kt
@@ -4,6 +4,7 @@ import com.nhlstenden.morithij.budgettracker.models.*
 import com.nhlstenden.morithij.budgettracker.models.dao.DAO
 import com.nhlstenden.morithij.budgettracker.models.dao.DAOFactory
 import com.nhlstenden.morithij.budgettracker.models.dao.TotalMoneyDAO
+import javafx.application.Platform
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
 import javafx.scene.control.Label
@@ -12,43 +13,52 @@ import java.lang.reflect.Field
 
 class OverviewController : Controller(), Observer {
     lateinit var records: List<MoneyRecordModel>
-    lateinit var total : TotalMoneyModel
-    lateinit var dao: DAO<TotalMoneyModel>
+    lateinit var total: TotalMoneyModel
 
     @FXML
     lateinit var totalMoneyLabel: Label
 
     fun initialize() {
         //TODO use MoneyRecordsDOA to get all records
-
-        dao = DAOFactory.getDAO(TotalMoneyModel::class.java) as DAO<TotalMoneyModel>
-        total = getTotalAmount()
-        total.addObserver(this)
-        totalMoneyLabel.text = "Total Budget:$total"
-
+        setTotalAmount()
     }
 
     override fun update(obj: Any) {
         if (obj is TotalMoneyModel) {
-            totalMoneyLabel.text = "Total Budget: $total"
-            dao.update(total)
+            val thread = Thread {
+                val dao = DAOFactory.getDAO(TotalMoneyModel::class.java) as DAO<TotalMoneyModel>
+                dao.update(total)
+                Platform.runLater {
+                    totalMoneyLabel.text = "Total Budget: $total"
+                }
+            }
+            thread.start()
         }
     }
 
-    fun getTotalAmount() : TotalMoneyModel{
-       // dao.save(TotalMoneyModel(1, 2.2))
-       // val record = dao.get(1)
-        return TotalMoneyModel(1, 2.2)
+    fun setTotalAmount() {
+        val thread = Thread {
+            val dao = DAOFactory.getDAO(TotalMoneyModel::class.java) as DAO<TotalMoneyModel>
+            //val record = dao.get(1)
+            val record = TotalMoneyModel(1, 2.2)
+            //TODO handle no total
+            if (record != null) {
+                total = record
+                onTotalInitialized()
+            }
+        }
+        thread.start()
+    }
+
+    private fun onTotalInitialized(){
+        total.addObserver(this)
+        totalMoneyLabel.text = "Total Budget:$total"
     }
 
     override fun setModels(vararg models: Any) {
-        val fields: Array<Field> = this.javaClass.declaredFields
-        for (field in fields) {
-            for (model in models) {
-                if (model.javaClass == field.type) {
-                    field.isAccessible = true
-                    field.set(this, model)
-                }
+        models.forEach {
+            if (it is TotalMoneyModel) {
+                total = it
             }
         }
     }
